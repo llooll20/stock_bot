e87909a7000c4a421f28a86f3edc2ea1131330ad
ItsmeLee <arsmagica90@gmail.com>
2024-11-25 15:52:09 +0900

    React 차트 띄우기 완료
    React 차트 띄우기 기능 구현 완료됨.

--- client/src/components/Chart.jsx
+++ client/src/components/Chart.jsx
@@ -1,14 +1,35 @@
-import fakeDB from "../fakeDB.json";
-import ApexCharts from "react-apexcharts";
-function Chart() {
-  return (
-    <ApexCharts
+import ApexChart from "react-apexcharts";
+import { useEffect, useState } from "react";
+
+const Chart = () => {
+  const [chartData, setChartData] = useState([]);
+  const [isLoading, setLoading] = useState(true);
+  const getChartData = async () => {
+    const json = await (await fetch("http://localhost:5000/")).json();
+    const result = json.splice(1);
+    setChartData(result);
+    setLoading(false);
+  };
+  useEffect(() => {
+    getChartData();
+  }, []);
+
+  return isLoading ? (
+    <h1>isLoading...</h1>
+  ) : (
+    <ApexChart
       type="candlestick"
       height="500"
       series={[
         {
-          data: fakeDB.map((item) => {
-            return [item.date, item.open, item.close, item.high, item.low];
+          data: chartData.map((item) => {
+            return [
+              item.date,
+              Number(item.high_price.split(",").join("")),
+              Number(item.low_price.split(",").join("")),
+              Number(item.opening_price.split(",").join("")),
+              Number(item.closing_price.split(",").join("")),
+            ];
           }),
         },
       ]}
@@ -33,8 +54,8 @@ function Chart() {
           type: "datetime",
         },
       }}
-    ></ApexCharts>
+    ></ApexChart>
   );
-}
+};
 
 export default Chart;
