f324da0f4a3e4a31ed8d700c00a8e0014a873aa9
ItsmeLee <arsmagica90@gmail.com>
2024-12-06 17:19:40 +0900

    로그인 폼 디자인 및 동작기능

--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -1,7 +1,107 @@
+import { useState } from "react";
 import "./styles/Signup.css";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 function SignUp() {
-  return <h1>Sign Up</h1>;
+  const navigate = useNavigate();
+
+  const [formData, setFormData] = useState({
+    nickname: "",
+    passowrd: "",
+    confirmPassword: "",
+  });
+
+  const [error, setError] = useState("");
+  const [success, setSuccess] = useState("");
+
+  const handleChange = (e) => {
+    setFormData((formData) => {
+      return {
+        ...formData,
+        [e.target.name]: e.target.value,
+      };
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError("");
+    setSuccess("");
+    if (formData.passowrd !== formData.confirmPassword) {
+      console.log(
+        typeof formData.passowrd,
+        typeof formData.confirmPassword,
+        formData.passowrd,
+        formData.confirmPassword,
+        formData.nickname
+      );
+      setError("비밀번호가 일치하지 않습니다");
+      return;
+    }
+    try {
+      const res = await axios.post(
+        "http://localhost:5000/api/signup",
+        {
+          nickname: formData.nickname,
+          passowrd: formData.passowrd,
+        },
+        { withCredentials: true }
+      );
+      setSuccess(res.data.message);
+      navigate("/login"); // login으로 이동
+    } catch (error) {
+      if (
+        error.response &&
+        error.response.data &&
+        error.response.data.message
+      ) {
+        setError(error.response.data.message);
+      } else {
+        setError("회원 가입 중 오류");
+      }
+    }
+  };
+  return (
+    <div className="signUp_container">
+      {error && <p className="error">{error}</p>}
+      {success && <p className="success">{success}</p>}
+      <form className="signUp_form_container" onSubmit={handleSubmit}>
+        <div>
+          <input
+            type="text"
+            name="nickname"
+            value={formData.nickname || ""}
+            placeholder="사용할 아이디 입력"
+            onChange={handleChange}
+            required
+          />
+        </div>
+
+        <div>
+          <input
+            type="password"
+            name="password"
+            value={formData.password || ""}
+            placeholder="비밀번호 입력"
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <input
+            type="password"
+            name="confirmPassword"
+            value={formData.confirmPassword || ""}
+            placeholder="비밀번호 확인"
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <button type="submit">회원가입</button>
+      </form>
+    </div>
+  );
 }
 
 export default SignUp;
